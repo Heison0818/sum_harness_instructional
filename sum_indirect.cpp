7947571a4375220aad3b0c987178f63b87954b88
Heison0818 <jhu7@mail.sfsu.edu>
2025-03-28 23:16:12 -0700

    2

--- sum_indirect.cpp
+++ sum_indirect.cpp
@@ -34,7 +34,7 @@ sum(int64_t N, double A[])
    printf(" inside sum_indirect perform_sum, N=%lld \n", N);
    
    double result = 0.0;
-   for (int64_t i = 0; i < N; ) {
+   for (int64_t i = 0; i < N; ++i) {
       result += A[indices[i]];
    }
 
